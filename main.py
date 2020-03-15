c3c38f79300e7b9573bbd9f036f69ea078aeb42e
moon qiu <moonqiu@moondeMacBook-Pro.local>
2020-03-15 14:07:59 +0800

    feat:#main,follow

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 import sys
 sys.path.append("../follow_trade")
 
-from exchange.okex.okexFutureApiV3 import OKEXFutureService as okf
+from api.okex.okexFutureApiV3 import OKEXFutureService as okf
+from util.general import Log
 
 from pydash import collections
 import pandas as pd
@@ -49,12 +50,12 @@ class Follow:
         #import pdb; pdb.set_trace()
         self.follow_account_codes = json.loads(cf.get(self.strategy,"follow_account_codes"))
 
-        self.contractType = cf.get(self.strategy,"contractType")
+        self.contract_type = cf.get(self.strategy,"contractType")
         self.trade_amount = cf.get(self.strategy,"trade_amount")
         self.rates = json.loads(cf.get(self.strategy,"rates"))
         self.contractMultiple = int(cf.get(self.strategy,"contractMultiple"))
         self.default_data["symbol"] = self.symbol
-        self.default_data["contract_type"] = self.contractType
+        self.default_data["contract_type"] = self.contract_type
         self.default_data["lever_rate"] = 20 if self.contractMultiple == 1 else 10
 
         self.trade_api = okf(account=self.trade_account_code)
@@ -65,8 +66,8 @@ class Follow:
 
 
     def judge_account(self):
-        trade_account = self.trade_api.accountInfo(self.coin)
-        follow_account = self.follow_api_1.accountInfo(self.coin) # 第一个能获取到就行，其他不管
+        trade_account = self.trade_api.getAccountInfo(self.symbol,self.contract_type)
+        follow_account = self.follow_api_1.getAccountInfo(self.symbol,self.contract_type) # 第一个能获取到就行，其他不管
         if not trade_account.has_key(self.coin) or not follow_account.has_key(self.coin):
             return
         self.handler_account(trade_account.get(self.coin),self.trade_account_code)
@@ -95,7 +96,7 @@ class Follow:
     def handler_account(self,accountInfo,account_code):
         data= {}
         data["rights"] = float(accountInfo["rights"])
-        c_info = accountInfo.get(self.contractType,{})
+        c_info = accountInfo.get(self.contract_type,{})
         data["profit"] = float(c_info.get("profit",0))
         data["unprofit"] = float(c_info.get("unprofit",0))
         data["freeze"] = float(c_info.get("freeze",0))
@@ -103,25 +104,25 @@ class Follow:
         self.show_info(data,Type="account",account=account_code)
 
     def judge_position(self):
-        trade_position = self.trade_api.position(self.symbol,contractType=self.contractType,Type=self.contractMultiple)
-        follow_position = self.follow_api_1.position(self.symbol,contractType=self.contractType,Type=self.contractMultiple)
+        trade_position = self.trade_api.position(self.symbol,contractType=self.contract_type,Type=self.contractMultiple)
+        follow_position = self.follow_api_1.position(self.symbol,contractType=self.contract_type,Type=self.contractMultiple)
         
-        self.trade_position = trade_position.get(self.contractType,{})
+        self.trade_position = trade_position.get(self.contract_type,{})
         if self.trade_position:
             self.show_info(self.trade_position,Type="position",account=self.trade_account_code)
         else:
             # 空仓，填默认值
             self.show_info(self.default_data,Type="position",account=self.trade_account_code)
         
-        self.follow_position_1 = follow_position.get(self.contractType,{})
+        self.follow_position_1 = follow_position.get(self.contract_type,{})
         if self.follow_position_1:
             self.show_info(self.follow_position_1,Type="position",account=self.follow_account_codes[0])
         else:
             self.show_info(self.default_data,Type="position",account=self.follow_account_codes[0])
         
         for c in range(2,self.follow_account_codes.__len__() + 1):
-            follow_position = eval("self.follow_api_%s.position(self.symbol,contractType=self.contractType,Type=self.contractMultiple)"%c)
-            self.__dict__["follow_position_%s"%c] = follow_position.get(self.contractType,{})
+            follow_position = eval("self.follow_api_%s.position(self.symbol,contractType=self.contract_type,Type=self.contractMultiple)"%c)
+            self.__dict__["follow_position_%s"%c] = follow_position.get(self.contract_type,{})
             return True
         return True
 
@@ -147,7 +148,7 @@ class Follow:
 
 
     def _do_trade(self,follow_position,follow_api,rate,Type=None):
-        ticker = follow_api.tickers(self.symbol,contractType=self.contractType)
+        ticker = follow_api.tickers(self.symbol,contractType=self.contract_type)
         if not ticker or not ticker.has_key("buyOne"):
             return
         if Type == "buy":
@@ -170,24 +171,27 @@ class Follow:
             #import pdb;pdb.set_trace()
             insert = False
             if compare_price * 0.99 < self.trade_position.get("%s_price_avg"%Type,0.0) and Type == "buy":
-                res = follow_api.buy(self.symbol,compare_price,diff_amount,contractType=self.contractType,matchPrice=True)
+                res = follow_api.buy(self.symbol,compare_price,diff_amount,contractType=self.contract_type,matchPrice=True)
                 print "%s加仓: %s"%(Type,res)
                 insert = True
             if compare_price * 1.01 > self.trade_position.get("%s_price_avg"%Type,0.0) and Type == "sell":
-                res = follow_api.sell(self.symbol,compare_price,diff_amount,contractType=self.contractType,matchPrice=True)
+                res = follow_api.sell(self.symbol,compare_price,diff_amount,contractType=self.contract_type,matchPrice=True)
                 print "%s加仓: %s"%(Type,res)
                 insert = True
             if insert:
                 data["msg"] = "加 %s仓: 价格： %s  数量： %s"%("多" if Type == "buy" else "空",compare_price,abs(diff_amount)/rate)
                 data["result"] = 0
+                # 插入log#
+                Log(self.coin,data["msg"])
         elif diff_amount < 0:
             # 需要减仓 直接市价减
             diff_amount = abs(diff_amount)
-            res = eval("follow_api.liqui_%s(self.symbol,compare_price,diff_amount,contractType=self.contractType,matchPrice=True)"%(Type))
+            res = eval("follow_api.liqui_%s(self.symbol,compare_price,diff_amount,contractType=self.contract_type,matchPrice=True)"%(Type))
             print "减仓: 数量: %s  结果: %s"%(diff_amount,res)
             data["msg"] = "减 %s仓: 价格： %s  数量： %s"%("多" if Type == "buy" else "空",compare_price,diff_amount/rate)
             coin_count = round(diff_amount/compare_price/20,4)
             data["result"] = 1*coin_count if self.trade_position.get("%s_profit_lossratio"%Type) > 0 else -1 *coin_count
+            Log(self.coin,data["msg"])
 
 
     def update_trade_amount(self, price, amount, profit_rate, Type=None):
@@ -200,10 +204,12 @@ class Follow:
             coin_diff = round(diff_amount*10/price/20,4)
             data["result"] = int(1 * coin_diff) if profit_rate >0 else int(-1 * coin_diff)
             self.__dict__["last_%s_trade_amount"%Type] = amount
+            Log(self.coin,data["msg"])
             return
         if temp_last_amount < amount:
             diff_amount = amount - temp_last_amount
             data["msg"] = "加 %s仓: 价格: %s  数量： %s"%("多" if Type == "buy" else "空",price,diff_amount)
             data["result"] = 0
             self.__dict__["last_%s_trade_amount"%Type] = amount
+            Log(self.coin,data["msg"])
             return
